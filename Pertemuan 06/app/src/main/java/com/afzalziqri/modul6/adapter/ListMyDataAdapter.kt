9243de4856b79ad5995d1915399e333bb96fe2e1
Afzal ziqri <57381362+afzalziqri@users.noreply.github.com>
2022-02-03 21:24:08 +0700

    Update ListMyDataAdapter.kt

--- Pertemuan 06/app/src/main/java/com/afzalziqri/modul6/adapter/ListMyDataAdapter.kt
+++ Pertemuan 06/app/src/main/java/com/afzalziqri/modul6/adapter/ListMyDataAdapter.kt
@@ -6,8 +6,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.rohmanbeny.modul6.MyData
-import com.rohmanbeny.modul6.R
+import com.afzalziqri.modul6.MyData
+import com.afzalziqri.modul6.R
 import kotlinx.android.synthetic.main.item_list.view.*
 
 class ListMyDataAdapter(private val listMyData: ArrayList<MyData>):
