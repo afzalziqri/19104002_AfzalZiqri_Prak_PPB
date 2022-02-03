d0dff78d53a6b153476a7fadbfd2f4bc40000101
Afzal ziqri <57381362+afzalziqri@users.noreply.github.com>
2022-02-03 22:37:23 +0700

    Update ExampleInstrumentedTest.kt

--- Pertemuan 10/app/src/androidTest/java/com/afzalziqri/modul10/ExampleInstrumentedTest.kt
+++ Pertemuan 10/app/src/androidTest/java/com/afzalziqri/modul10/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.rifqiakmals_19104022.modul10
+package com.afzalziqri.modul10
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
