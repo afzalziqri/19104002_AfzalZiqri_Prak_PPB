f2428aa04b64712c8d724b02d9e2a79debb31d0f
Afzal ziqri <57381362+afzalziqri@users.noreply.github.com>
2022-02-03 20:58:11 +0700

    Update ExampleInstrumentedTest.kt

--- Pertemuan 03/lifecycleapp/app/src/androidTest/java/com/afzalziqri/lifecycleapp/ExampleInstrumentedTest.kt
+++ Pertemuan 03/lifecycleapp/app/src/androidTest/java/com/afzalziqri/lifecycleapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.rohmanbeny.lifecycleapp
+package com.afzalziqri.lifecycleapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
